622532fa0339ecb252a562360b8cefa7b821b437
ugotchriscoded <chrisdobadoglez@gmail.com>
2024-11-25 11:38:27 +0100

    docs: modify schema

--- database/migrations/2024_11_25_095505_create_jobsearch_table.php
+++ database/migrations/2024_11_25_095505_create_jobsearch_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('jobsearch', function (Blueprint $table) {
             $table->id();
+            $table->text("entry");
+            $table->string("emotions");
             $table->timestamps();
         });
     }
