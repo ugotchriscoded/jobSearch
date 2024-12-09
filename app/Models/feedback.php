42a35fdf863d3adbc71a0994e0d43d6d5f74c663
ugotchriscoded <chrisdobadoglez@gmail.com>
2024-12-09 12:04:15 +0100

    feat: models feedback update

--- app/Models/feedback.php
+++ app/Models/feedback.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class feedback extends Model
+class Feedback extends Model
+
 {
-    //
+    use HasFactory;
+
+    protected $fillable =
+    [
+        'offer_id',
+        'news'
+    ];
+
+    public function offer()
+    {
+        return $this->belongsTo(Offer::class);
+    }
+
 }
