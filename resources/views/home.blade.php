f96ba23f76e56cd4b6f2eac7c45040e6d4784f7e
ugotchriscoded <chrisdobadoglez@gmail.com>
2024-12-09 12:22:43 +0100

    feat: home blade views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,36 @@
 @extends('layouts.app')
+
 @section('content')
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Date</th>
-        <th scope="col">Last Update</th>
-        <th scope="col">Company</th>
-        <th scope="col">Apply</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
+
+
+    <div class="offers">
+        <table class="table">
+            <th>
+                <tr class="t" >
+                    <th>Title</th>
+                    <th>Enterprise</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th>Applied</th>
+                </tr>
+            </th>
+            <tb>
+                @foreach ($offers as $offer)
+                        <tr class="row" data-href="{{route('show', $offer->id)}}">
+                            <td>{{$offer->title}}</td>
+                            <td>{{$offer->enterprise}}</td>
+                            <td>{{$offer->description}}</td>
+                                <td>
+                                    @if ($offer->status===1)
+                                    ✅
+                                    @else 
+                                    ❎
+                                    @endif
+                                </td>
+                            <td>{{$offer->created_at}}</td>
+                        </tr>    
+                @endforeach
+            </tb>
+        </table>
+    </div>
 @endsection
