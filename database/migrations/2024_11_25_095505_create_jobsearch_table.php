7234eef433a0003dd1dc8a5a346b17c60132dde7
ugotchriscoded <chrisdobadoglez@gmail.com>
2024-12-09 11:53:39 +0100

    fix: offers table

--- database/migrations/2024_11_25_095505_create_jobsearch_table.php
+++ database/migrations/2024_11_25_095505_create_jobsearch_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
-            $table->string("offers");
-            $table->string("updates");
+            $table->string('title');
+            $table->string('enterprise');
+            $table->text('description');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
@@ -24,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jobsearch');
+        Schema::dropIfExists('offers');
     }
 };
